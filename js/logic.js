e3d53a446d598bfea26e51b00eb579d485e30d27
snayps <popovsny@gmail.com>
2012-12-10 22:40:50 +0200

    Коммит без Б=)

--- js/logic.js
+++ js/logic.js
@@ -1,17 +1,17 @@
 $(function(){
-			var $frames = $(".b-accordion .b-accordion__item");
+			var $frames = $(".accordion .accordion__item");
 
-			$(".b-accordion__item-name").on("click", function(){
+			$(".accordion__item-name").on("click", function(){
 
 				var $this = $(this),
-				$target = $this.next(".b-accordion__item-text");
+				$target = $this.next(".accordion__item-text");
 
 				if ($this.parent().hasClass("active")) {
 					$target.slideUp();
 					$this.parent().removeClass("active");
 
 				}else{
-					$frames.children(".b-accordion__item-text").slideUp();
+					$frames.children(".accordion__item-text").slideUp();
 					$frames.removeClass("active");
 					$target.slideDown();
 					$(this).parent().addClass("active");
